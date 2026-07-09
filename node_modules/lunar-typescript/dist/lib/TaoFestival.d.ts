90b7c0a209abfd5dc56cf1f769ec947f3467bf1b
CherryClaw <cherryclaw@users.noreply.github.com>
2026-07-09 18:04:27 +0800

    爻一爻 v1.0 - 八字紫微六爻梅花风水面相纳音解梦灵签

--- node_modules/lunar-typescript/dist/lib/TaoFestival.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class TaoFestival {
-    private readonly _name;
-    private readonly _remark;
-    constructor(name: string, remark?: string);
-    getName(): string;
-    getRemark(): string;
-    toString(): string;
-    toFullString(): string;
-}
